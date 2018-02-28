c83e5f2cac8ce84b03c473ccc7d10a1f85f61edd
Jonathan Thom <jonathan.thom1990@gmail.com>
2018-02-27 22:58:01 -0800

    Add twilio error catch

--- app/services/twilio/client.rb
+++ app/services/twilio/client.rb
@@ -9,11 +9,15 @@ class Twilio::Client
     auth_token = ENV["TWILIO_AUTH_TOKEN"]
     body = News::Nyt::Client.new.run
 
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
-    message = @client.messages.create(
-      body: body,
-      to: ENV["MY_PHONE_NUMBER"],
-      from: ENV["TWILIO_PHONE_NUMBER"]
-    )
+    begin
+      @client = Twilio::REST::Client.new(account_sid, auth_token)
+      message = @client.messages.create(
+        body: body,
+        to: ENV["MY_PHONE_NUMBER"],
+        from: ENV["TWILIO_PHONE_NUMBER"]
+      )
+    rescue Twilio::REST::TwilioError => e
+      puts e.message
+    end
   end
 end
